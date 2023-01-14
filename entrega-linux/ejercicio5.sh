edf3507d54b24a55d20ac86c665ac50809c9ddbe
Salvador Rioboo Naranjo <salrio74@gmail.com>
2023-01-14 18:38:27 +0100

    feat: completado ejercicio5.sh

--- entrega-linux/ejercicio5.sh
+++ entrega-linux/ejercicio5.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-PROTOCOLO='https://'
-
 # verifica parametros
-if [ $# -lt 2 ]: then
+if [ $# -ne 2 ]; then
 	echo "Se necesitan únicamente dos parámetros para ejecutar este script"
-fi
+else
+	# obtener la ruta
+	curl -s $1 -o temp.txt
 
-# obtener la ruta
-curl -s $1 -o temp.txt
-
-# buscar la palabra en el fichero
-
-
-if [ $# != 0 ]; then
-	COUNT=`grep $2 -c temp.txt`
-	FIRST_LINE=`grep -n $2 temp.txt | head -n1 | awk -F: '{ print $1 }'`
-	if [ $COUNT != 0 ]; then
-		echo "La palabra \"$2\" se ha encontrado $COUNT veces"
-		echo "Aparece por primera vez en la línea $FIRST_LINE"
-	else	
-		echo "No se ha encontrado la palabra \"$2\""
+	# buscar la palabra en el fichero
+	if [ $# != 0 ]; then
+		COUNT=`grep $2 -c temp.txt`
+		FIRST_LINE=`grep -n $2 temp.txt | head -n1 | awk -F: '{ print $1 }'`
+		if [ $COUNT == 0 ]; then
+			echo "No se ha encontrado la palabra \"$2\""
+		elif [ $COUNT == 1 ]; then
+			echo "La palabra \"$2\" aparece $COUNT vez"
+			echo "Aparece unicamente en la línea $FIRST_LINE"
+		else	
+			echo "La palabra \"$2\" se ha encontrado $COUNT veces"
+			echo "Aparece por primera vez en la línea $FIRST_LINE"
+		fi
 	fi
 fi
